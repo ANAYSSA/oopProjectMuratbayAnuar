fbd8621f8b08c1358f5921ebace83f2e66963ed7
Anuar <kacrdbgs1423@gmail.com>
2026-01-24 23:41:55 +0500

    Initial commit

--- src/anuarproject/oop1/FilmHandler.java
+++ src/anuarproject/oop1/FilmHandler.java
@@ -1,43 +1,28 @@
 package anuarproject.oop1;
-import com.sun.net.httpserver.HttpHandler;
+
 import com.sun.net.httpserver.HttpExchange;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
-public class FilmHandler implements HttpHandler {
+import java.util.stream.Collectors;
+import java.io.IOException;
+public class FilmHandler implements BaseHandler { // Наследуем наш интерфейс
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        if ("GET".equals(exchange.getRequestMethod())) {
-            try {
-                List<Film> films = DBManager.getInstance().getAllFilms();
-                StringBuilder json = new StringBuilder("[");
-                for (int i = 0; i < films.size(); i++) {
-                    Film f = films.get(i);
-                    json.append("{")
-                            .append("\"title\":\"").append(f.getTitle()).append("\",")
-                            .append("\"price\":").append(f.getPrice()).append(",")
-                            .append("\"genre\":\"").append(f.getGenre()).append("\"")
-                            .append("}");
-                    if (i < films.size() - 1) json.append(",");
-                }
-                json.append("]");
+        if (!"GET".equals(exchange.getRequestMethod())) {
+            exchange.sendResponseHeaders(405, -1);
+            return;
+        }
 
-                String response = json.toString();
+        try {
+            List<Film> films = DBManager.getInstance().getAllFilms();
 
-                exchange.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
-                exchange.sendResponseHeaders(200, response.getBytes().length);
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-                String error = "Internal Server Error";
-                exchange.sendResponseHeaders(500, error.length());
-                exchange.getResponseBody().write(error.getBytes());
-                exchange.getResponseBody().close();
-            }
-        } else {
-            exchange.sendResponseHeaders(405, -1); // 405 Method Not Allowed
+            String json = "[" + films.stream()
+                    .map(f -> String.format("{\"title\":\"%s\",\"price\":%.2f,\"genre\":\"%s\"}",
+                            f.getTitle(), f.getPrice(), f.getGenre()))
+                    .collect(Collectors.joining(",")) + "]";
+
+            sendJsonResponse(exchange, json, 200);
+        } catch (Exception e) {
+            sendJsonResponse(exchange, "{\"error\":\"" + e.getMessage() + "\"}", 500);
         }
     }
 }
