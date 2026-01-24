7e90e885ac14a4e487419a2a6783aaf0a57891b2
Anuar <kacrdbgs1423@gmail.com>
2026-01-24 23:23:25 +0500

    Initial commit

--- src/anuarproject/oop1/CinemaServer.java
+++ src/anuarproject/oop1/CinemaServer.java
@@ -9,8 +9,9 @@ public class CinemaServer {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/api/films", new FilmHandler());
         server.createContext("/", new StaticHandler());
+        server.createContext("/api/login", new LoginHandler());
         server.setExecutor(null);
-        System.out.println("http://localhost:8080/api/films");
+        System.out.println("http://localhost:8080/");
         server.start();
     }
 }
